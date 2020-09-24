e4473608d71b7d503a462a053927e978e1d4644e
Michiel-PC <michielbos01@gmail.com>
2020-09-24 15:19:13 +0200

    first commit

---
+++ src/Output.java
@@ -0,0 +1,23 @@
+
+
+public class Output {
+}
+
+    public static void main(String[] args) {
+
+    import java.util.Scanner;
+
+        System.out.println("voer een woord in");
+        Scanner s = new Scanner(System.in);
+        var tekst = s.nextLine();
+        System.out.println(tekst.length);
+        System.out.println("voer een getal in");
+        int getal = invoer.nextInt();
+        System.out.println("de tafel van 288 is:");
+        int uitkomst1 = getal*1;
+        int uitkomst2 = getal*2;
+        System.out.println("1x  " +getal "= " +uitkomst1 );
+        System.out.println("2x  " +getal "= " +uitkomst2 );
+
+
+    }
